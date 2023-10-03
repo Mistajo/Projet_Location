04f34378dc690e65b8a4eaa7daa6b1a5e093de97
Mistajo <130221389+Mistajo@users.noreply.github.com>
2023-10-03 10:49:50 +0200

    Mise en place vehicule user

--- src/Controller/User/Vehicle/VehicleController.php
+++ src/Controller/User/Vehicle/VehicleController.php
@@ -2,18 +2,22 @@
 
 namespace App\Controller\User\Vehicle;
 
+use App\Entity\Agency;
+use App\Repository\AgencyRepository;
 use App\Repository\VehicleRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class VehicleController extends AbstractController
 {
     #[Route('/user/vehicle', name: 'user.vehicle.index')]
-    public function index(VehicleRepository $vehicleRepository): Response
+    public function index(VehicleRepository $vehicleRepository, AgencyRepository $agency): Response
     {
+        $vehicles = $vehicleRepository->findBY(['isAvailable' => true], ['availableAt' => 'DESC']);
         return $this->render('pages/user/vehicle/index.html.twig', [
-            'vehicles' => $vehicleRepository->findAll()
+            'vehicles' => $vehicles,
+            'agencies' => $agency->findAll(),
         ]);
     }
 }
